9f61fa9028d341f9eb73d6cc62fa6a6fd36503ff
Juho Härme <juho.harme@uta.fi>
2017-06-19 11:55:52 +0300

    Start analysis

--- teksti/db_vars.R
+++ teksti/db_vars.R
@@ -1,7 +1,7 @@
 library(RSQLite)
 con <- dbConnect(RSQLite::SQLite(), "~/data/matkakertomukset.db")
-#paragraphs <- dbGetQuery(con, "SELECT parsed, theme, content, uncertain, theme FROM paragraphs WHERE theme_j!='' AND theme_k!='' AND content !=''")
-paragraphs <- dbGetQuery(con, "SELECT content FROM paragraphs WHERE content !=''")
+paragraphs <- dbGetQuery(con, "SELECT textid, parsed, theme, content, uncertain, theme, analyzedby FROM paragraphs WHERE theme !='' AND content !=''")
+#paragraphs <- dbGetQuery(con, "SELECT content FROM paragraphs WHERE content !=''")
 dbDisconnect(con)
 saveRDS(paragraphs, "paragraphs.RDS")
 
