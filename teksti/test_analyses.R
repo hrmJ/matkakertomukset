a02f1150c66025f0b153c1437518037987291815
hrmJ <juho.harme@uta.fi>
2017-06-19 15:28:52 +0300

    some plots

---
+++ teksti/test_analyses.R
@@ -0,0 +1,13 @@
+# Load the relevant model into R’s working memory:
+source("r/DBDA2E-utilities.R") 
+source("r/kruschke_anova.R") 
+# Generate the MCMC chain:
+ss <- headverbs[,c("sentence_number","pers")]
+
+mcmcCoda = genMCMC(datFrm=ss, yName="sentence_number", xName="pers", numSavedSteps=11000, thinSteps=10, saveName="somefile")
+
+#parameterNames = varnames(mcmcCoda) 
+#show(parameterNames) 
+#for (parName in parameterNames) { 
+#    diagMCMC(codaObject=mcmcCoda, parName=parName, saveName="somefile", saveType="png")
+#} 
