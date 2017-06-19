3f943c97405e0eefbe389fb6f8e1605ba7f4a4c7
hrmJ <juho.harme@uta.fi>
2017-06-19 16:51:45 +0300

    muokkaus

--- teksti/test_analyses.R
+++ teksti/test_analyses.R
@@ -2,6 +2,11 @@
 source("r/DBDA2E-utilities.R") 
 source("r/kruschke_anova.R") 
 # Generate the MCMC chain:
-ss <- headverbs[,c("sentence_number","pers")]
-mcmcCoda = genMCMC(datFrm=ss, yName="sentence_number", xName="pers", numSavedSteps=11000, thinSteps=10)
-mustacheplot <- plotMCMC(mcmcCoda,ss,"sentence_number","pers")
+ss <- headverbs[headverbs$number_of_paragraphs>1,c("paragraph_number","number_of_paragraphs","pers")]
+ss$paragraph_number <- ss$paragraph_number/ss$number_of_paragraphs
+yName="paragraph_number";xName="pers"
+#ss$sentence_number <- headverbs$sentence_number/headverbs$number_of_sentences
+mcmcCoda = genMCMC(datFrm=ss, yName=yName, xName=xName, numSavedSteps=11000, thinSteps=10)
+plotMCMC(mcmcCoda,ss,yName,xName)
+
+
